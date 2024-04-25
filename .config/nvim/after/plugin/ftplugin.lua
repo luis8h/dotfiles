b252ce12683dac710894ea7e22d96aacc1e55b4b
luis8h <vzbls@t-online.de>
2024-04-25 07:51:08 +0200

    fixed terraform .tfvars errors

--- .config/nvim/after/plugin/ftplugin.lua
+++ .config/nvim/after/plugin/ftplugin.lua
@@ -1,10 +1,32 @@
 
--- adds a custom filetype for .asy files, so asy.lua is executed when opening such file
+-------------------------------------------------------------------------------
+--- ASYMPTOTE -----------------------------------------------------------------
+-------------------------------------------------------------------------------
+--adds a custom filetype for .asy files, so asy.lua is executed when opening such file
 vim.filetype.add({
   extension = {
     asy = 'asy', -- This line adds a custom filetype for the extension .asy
   },
 })
+
+
+-------------------------------------------------------------------------------
+--- TERRAFORM -----------------------------------------------------------------
+-------------------------------------------------------------------------------
+
+-- Define function to set Terraform filetype
+_G.set_terraform_filetype = function ()
+    vim.bo.filetype = "terraform"
+end
+-- workaround for .tfvars file to work without error by setting the filetype to terraform automaticly
+vim.cmd([[
+  augroup TerraformFileType
+    autocmd!
+    autocmd BufNewFile,BufRead *.tf setfiletype terraform
+    autocmd BufNewFile,BufRead *.tfvars lua set_terraform_filetype()
+    autocmd BufNewFile,BufRead *.tfstate setfiletype json
+  augroup END
+]])
 
 -- fix for terraform lsp working
 vim.filetype.add({
