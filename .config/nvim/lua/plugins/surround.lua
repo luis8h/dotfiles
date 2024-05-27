f9853ddde16cae5fe3195a7d209eb6edb4ff2530
Luis Schmidmeister <vzbls@t-online.de>
2024-05-27 18:50:07 +0200

    changed surround shortcut from T to Z to avoid conflict with flash.nvim

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -5,19 +5,19 @@ return {
     config = function()
 
         require("nvim-surround").setup({
-            -- remap S key to make leap.nvim work
+            -- remap S key to make flash.nvim work
             keymaps = {
                 insert = "<C-g>s",
-                insert_line = "<C-g>T",
+                insert_line = "<C-g>Z",
                 normal = "ys",
                 normal_cur = "yss",
-                normal_line = "yT",
-                normal_cur_line = "yTT",
-                visual = "T",
-                visual_line = "gT",
+                normal_line = "yZ",
+                normal_cur_line = "yZZ",
+                visual = "Z",
+                visual_line = "gZ",
                 delete = "ds",
                 change = "cs",
-                change_line = "cT",
+                change_line = "cZ",
             },
             aliases = {
                 ["c"] = "```", -- could also be only for .md files
