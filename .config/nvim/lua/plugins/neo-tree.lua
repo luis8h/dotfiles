cf456a4a0c18edc43812c504a5a675d949b05105
luis8h <vzbls@t-online.de>
2024-06-27 07:47:25 +0200

    added neotree and whichkey

---
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,21 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    cmd = { "Neotree" },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons",
+        "MunifTanjim/nui.nvim",
+    },
+    config = function()
+        require("neo-tree").setup({
+            close_if_last_window = true,
+            filesystem = {
+                follow_current_file = {
+                    enabled = true,
+                    leave_dirs_open = true,
+                },
+            },
+        })
+    end,
+}
