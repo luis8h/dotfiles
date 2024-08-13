f1416efd2e079eaf0c8b90c4f3af01ce61f140f2
luis8h <vzbls@t-online.de>
2024-08-13 07:37:32 +0200

    display colors now working

--- .config/sketchybar/plugins/aerospace.sh
+++ .config/sketchybar/plugins/aerospace.sh
@@ -7,12 +7,25 @@ export DISPLAY_COLORS=("0xff94e2d5", "0xfffab387", "0xffa6e3a1", "0xffcba6f7")
 
 window_count=$(aerospace list-windows --workspace "$1" | wc -l)
 
+for monitor in $(aerospace list-monitors | awk '{print $1}'); do
+    for ws in $(aerospace list-workspaces --monitor "$monitor"); do
+        if [ "$ws" == "$1" ]; then
+            monitor_id="$monitor"
+            break
+        fi
+    done
+    if [ -n "$monitor_id" ]; then
+        break
+    fi
+done
+
+
 if [ "$window_count" -eq 0 ]; then
     sketchybar --set $NAME display=0
 else
     sketchybar --set $NAME \
         display=1 \
-        background.border_color=${DISPLAY_COLORS[$2 - 1]} \
+        background.border_color=${DISPLAY_COLORS[$monitor_id - 1]} \
 
 fi
 
