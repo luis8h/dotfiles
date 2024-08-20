fb91abc58290cee25b133f11b3c5e0048201cdcf
luis8h <vzbls@t-online.de>
2024-08-20 10:16:36 +0200

    added config for blankline indent plugin

--- .config/nvim/lua/plugins/indend-blankline.lua
+++ .config/nvim/lua/plugins/indend-blankline.lua
@@ -1,7 +1,9 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
-    ---@module "ibl"
-    ---@type ibl.config
-    opts = {},
+    config = function()
+        require("ibl").setup{
+            scope = { enabled = false }
+        }
+    end
 }
