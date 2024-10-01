b69c1e36a85896f39c1f9ae447237ccf853ccf94
Luis Schmidmeister <vzbls@t-online.de>
2024-10-01 14:13:41 +0200

    added useless duck plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -30,4 +30,27 @@ return {
         end
     },
 
+    {
+        'tamton-aquib/duck.nvim',
+        config = function()
+            vim.api.nvim_create_user_command('HatchDuck', function()
+                require("duck").hatch("💩", 10)
+            end, {})
+
+            vim.api.nvim_create_user_command('HatchManyDucks', function(opts)
+                local num_ducks = tonumber(opts.args) or 1
+                for i = num_ducks, 1, -1 do
+                    require("duck").hatch("💩", 10)
+                end
+            end, { nargs = 1 })
+
+            vim.api.nvim_create_user_command('CookDuck', function()
+                require("duck").cook()
+            end, {})
+
+            vim.api.nvim_create_user_command('CookAllDucks', function()
+                require("duck").cook_all()
+            end, {})
+        end
+    }
 }
