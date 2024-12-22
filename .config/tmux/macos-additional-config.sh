c8431b5c1bdd6975d8b8a0c2247a52cd7e3f2801
Luis Schmidmeister <vzbls@t-online.de>
2024-12-22 10:25:49 +0100

    updated tmux config

--- .config/tmux/macos-additional-config.sh
+++ .config/tmux/macos-additional-config.sh
@@ -2,4 +2,6 @@
 
 if uname | grep -qi darwin; then
     echo "set -g @thumbs-command 'echo -n {} | pbcopy'"
+    echo 'bind-key -n MouseDragEnd1Pane run "tmux copy-mode -M \; tmux save-buffer - | pbcopy"'
+    echo 'bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel \; run "tmux save-buffer - | pbcopy"'
 fi
