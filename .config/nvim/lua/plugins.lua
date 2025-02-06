2cdafe44ffb68127a51f5bf28f208f9eea23631e
luis8h <vzbls@t-online.de>
2025-02-06 17:43:37 +0100

    added build directory to vimtex

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -26,9 +26,8 @@ return {
     {
         "lervag/vimtex",
         lazy = false,     -- we don't want to lazy load VimTeX
-        -- tag = "v2.15", -- uncomment to pin to a specific release
         init = function()
-            -- VimTeX configuration goes here
+            vim.g.vimtex_compiler_latexmk = { out_dir = "build" }
         end
     },
 
