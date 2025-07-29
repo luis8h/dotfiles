7cf293d170cba19fe3608a1d8bea714c8149e65f
luis8h <vzbls@t-online.de>
2025-07-29 07:32:08 +0200

    added lsp servers

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,7 +1,8 @@
 return {
     "mason-org/mason-lspconfig.nvim",
     opts = {
-        ensure_installed = { "lua_ls", "rust_analyzer", "jdtls", "gopls" },
+        ensure_installed = { "lua_ls", "rust_analyzer", "jdtls", "gopls", "ty", "ruff", "cssls", "html", "jsonls", "ts_ls" },
+        -- prettier, prettierd
     },
     dependencies = {
         { "mason-org/mason.nvim", opts = {} },
