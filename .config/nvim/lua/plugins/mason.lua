c1a0d07fc391d22ae63f4669f2b22520bd82e8b6
luis8h <vzbls@t-online.de>
2025-07-30 08:19:05 +0200

    fiexed lsps not being enabled

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,8 +1,6 @@
 return {
 	{
 		"mason-org/mason-lspconfig.nvim",
-		event = "VeryLazy",
-		cmd = { "Mason" },
 		dependencies = {
 			{ "mason-org/mason.nvim", opts = {} },
 		    "neovim/nvim-lspconfig",
@@ -15,36 +13,5 @@ return {
 		opts = {
             ensure_installed = { "lua_ls", "rust_analyzer", "jdtls", "gopls", "ty", "ruff", "cssls", "html", "jsonls", "ts_ls" },
 		},
-		config = function(_, opts)
-			----------------------------------
-			-- Server configuration
-			----------------------------------
-			-- Get server names from after/lsp
-			-- local servers = {}
-			-- local lsp_servers_path = vim.fn.stdpath("config") .. "/lsp"
-			--
-			-- for file in vim.fs.dir(lsp_servers_path) do
-			-- 	local name = file:match("(.+)%.lua$")
-			-- 	if name then
-			-- 		servers[name] = true
-			-- 	end
-			-- end
-			----------------------------------
-			-- Add servers automaticlly
-			----------------------------------
-			-- local ensure_installed = vim.tbl_keys(servers or {})
-			-- vim.list_extend(ensure_installed, opts.lsp_servers)
-			-- vim.list_extend(ensure_installed, opts.tools)
-			--
-			-- require("mason-lspconfig").setup({
-			-- 	ensure_installed = {},
-			-- })
-			--
-			-- require("mason-tool-installer").setup({
-			-- 	ensure_installed = ensure_installed,
-			-- 	auto_update = true,
-			-- 	run_on_start = true,
-			-- })
-		end,
 	},
 }
