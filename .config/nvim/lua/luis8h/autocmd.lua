6b2db09c17588e0543e98e80676acbc64561b359
luis8h <vzbls@t-online.de>
2025-07-31 07:28:47 +0200

    fixed pythong commend indenting

--- .config/nvim/lua/luis8h/autocmd.lua
+++ .config/nvim/lua/luis8h/autocmd.lua
@@ -18,8 +18,18 @@ autocmd('TextYankPost', {
 
 -- removes trailing whitespaces on save
 -- use `:noa w` to save without using this
-autocmd({"BufWritePre"}, {
+autocmd({ "BufWritePre" }, {
     group = luis8h_group,
     pattern = "*",
     command = [[%s/\s\+$//e]],
+})
+
+-- fix python command indenting not working
+vim.api.nvim_create_augroup("PythonIndentFix", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+    group = "PythonIndentFix",
+    pattern = "python",
+    callback = function()
+        vim.opt_local.smartindent = false
+    end,
 })
