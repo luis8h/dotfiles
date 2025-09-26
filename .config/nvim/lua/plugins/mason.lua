b81c2d8644fc94bb4c6ed772d2d57d76feb342b7
luis8h <vzbls@t-online.de>
2025-09-26 16:32:28 +0200

    added tailwind lsp

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -10,7 +10,7 @@ return {
             "b0o/SchemaStore.nvim",
         },
         opts = {
-            ensure_installed = { "lua_ls", "rust_analyzer", "jdtls", "gopls", "ty", "ruff", "cssls", "html", "jsonls", "ts_ls", "basedpyright", "ltex_plus", "texlab" },
+            ensure_installed = { "lua_ls", "rust_analyzer", "jdtls", "gopls", "ty", "ruff", "cssls", "html", "jsonls", "ts_ls", "basedpyright", "ltex_plus", "texlab", "tailwindcss" },
         }
     },
 }
