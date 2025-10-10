94e9730d5e5c291e12287d923149f92b9dee23b5
luis8h <luis.schmidmeister@trinnovative.de>
2025-10-10 09:00:25 +0200

    adjusted silicon config

--- .config/nvim/lua/plugins/nvim-silicon.lua
+++ .config/nvim/lua/plugins/nvim-silicon.lua
@@ -1,22 +1,34 @@
 return {
-    "michaelrommel/nvim-silicon",
-    lazy = true,
-    cmd = "Silicon",
-    config = function()
-        require("silicon").setup({
-            -- Configuration here, or leave empty to use defaults
-            font = "JetBrainsMono Nerd Font=34;Noto Color Emoji=34",
-            output = function ()
-                return '/' .. os.getenv("HOME") .. "/Pictures/" .. os.date("!%Y-%m-%dT%H-%M-%S") .. "_code.png"
-            end,
-            theme = 'Dracula',
-            window_title = function()
-                return vim.fn.fnamemodify(
+	"michaelrommel/nvim-silicon",
+	lazy = true,
+	cmd = "Silicon",
+	main = "nvim-silicon",
+	opts = {
+		-- Configuration here, or leave empty to use defaults
+        font = "JetBrainsMono Nerd Font=34;Noto Color Emoji=34",
+        theme = 'Dracula',
+        background = "#12121200",
+        output = function()
+            local home = os.getenv("HOME")
+            local path = string.format(
+                "%s/Pictures/code_%s.png",
+                home,
+                os.date("%Y-%m-%d_%H-%M-%S")
+            )
+            return path
+        end,
+        to_clipboard = true,
+        pad_horiz = 0,
+        pad_vert = 0,
+        shadow_blur_radius = 0,
+        shadow_offset_x = 0,
+        shadow_offset_y = 0,
+        shadow_color = nil,
+        window_title = function()
+            return vim.fn.fnamemodify(
                 vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()),
                 ":t"
-                )
-            end,
-        })
-    end
+            )
+        end,
+	}
 }
-
