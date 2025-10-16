d474ff87ad2ae718f187db3fc72478d062fb1ed1
luis8h <luis.schmidmeister@trinnovative.de>
2025-10-16 10:02:43 +0200

    added clipboard history to hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -4,3 +4,11 @@ spoon.FuzzySwitcher:start()
 
 hs.loadSpoon("MyImageSpoon")
 spoon.MyImageSpoon:bindHotkeys()
+
+hs.loadSpoon("ClipboardTool")
+-- spoon.ClipboardTool.show_copied_alert = false
+spoon.ClipboardTool.show_in_menubar = true
+spoon.ClipboardTool:bindHotkeys({
+  toggle_clipboard = { { "cmd", "ctrl" }, "v" }
+})
+spoon.ClipboardTool:start()
