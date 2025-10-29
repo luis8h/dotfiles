138bdbbd4464818283ff6acc9149f4c78306b4e8
luis8h <vzbls@t-online.de>
2025-10-29 07:37:20 +0100

    updated nvim plugins

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -1,13 +1,7 @@
+-- install without yarn or npm
 return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
-    config = function()
-        vim.g.mkdp_auto_close = 0
-        vim.keymap.set("n", "<leader>mp", function()
-            vim.cmd("MarkdownPreviewToggle")
-        end)
-    end
-};
-
+}
