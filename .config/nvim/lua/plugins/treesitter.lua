1dea83e9930bf2df0b4caffda37a8b3e22c614ad
luis8h <vzbls@t-online.de>
2026-03-21 13:50:13 +0100

    update treesitter and nvim

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,37 +1,27 @@
 return {
-
     'nvim-treesitter/nvim-treesitter',
-
-    build = function()
-        local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-        ts_update()
-    end,
-
+    lazy = false,
+    build = ':TSUpdate',
     config = function()
-
-        require'nvim-treesitter.configs'.setup {
-            -- A list of parser names, or "all"
+        require('nvim-treesitter').setup {
             ensure_installed = { "vimdoc", "javascript", "typescript", "c", "lua", "rust", "java", "python" },
-
-            -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
-
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
             auto_install = true,
-
             highlight = {
-                -- `false` will disable the whole extension
                 enable = true,
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
                 additional_vim_regex_highlighting = false,
-                disable = { "latex", },
+                disable = { "latex" },
+            },
+            indent = { enable = true },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "gnn",
+                    node_incremental = "grn",
+                    ccope_incremental = "grc",
+                    node_decremental = "grm",
+                },
             },
         }
-
     end,
 }
