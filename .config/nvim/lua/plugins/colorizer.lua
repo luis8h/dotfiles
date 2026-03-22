ea350dd83cd0837000214fab76eb3207ff3a2c66
luis8h <vzbls@t-online.de>
2026-03-22 11:39:04 +0100

    fix colorizer deprecated warning

--- .config/nvim/lua/plugins/colorizer.lua
+++ .config/nvim/lua/plugins/colorizer.lua
@@ -1,10 +1,11 @@
 -- https://github.com/norcalli/nvim-colorizer.lua
 -- This plugin allows me to see the colors of hex code inside files
-
 return {
-    "norcalli/nvim-colorizer.lua",
+    "catgoose/nvim-colorizer.lua",
+    event = "BufReadPre",
     config = function()
         require("colorizer").setup()
         vim.keymap.set("n", "<leader>cs", "<cmd>ColorizerToggle<cr>", { desc = "toggle colorizer" });
     end,
+    opts = {},
 }
