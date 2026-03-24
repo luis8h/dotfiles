7e7cb9b534a60e5c97155a698cf7307613d3bf8b
luis8h <vzbls@t-online.de>
2026-03-24 10:50:38 +0100

    fix treesitter hightlighting in tsx files

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -21,5 +21,20 @@ return {
                 'markdown', 'markdown_inline', 'vim', 'vimdoc', 'query', 'diff', 'git_config', 'gitcommit', 'gitignore'
             }
         )
+
+        -- IMPORTANT: In the 'main' branch of nvim-treesitter, you must manually trigger the highlighter.
+        -- This autocmd will start Tree-sitter highlighting whenever you open a supported file.
+        vim.api.nvim_create_autocmd('FileType', {
+            callback = function(args)
+                local buf = args.buf
+                local lang = vim.treesitter.language.get_lang(vim.bo[buf].filetype)
+
+                -- 1. Check if Neovim even knows this language
+                -- 2. Check if the parser is actually installed on your system
+                if lang and pcall(vim.treesitter.get_parser, buf, lang) then
+                    vim.treesitter.start(buf, lang)
+                end
+            end,
+        })
     end
 }
