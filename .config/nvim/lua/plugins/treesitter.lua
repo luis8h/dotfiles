9263b1eb0c3091199069263662d2e421451b73f0
luis8h <luis.schmidmeister@trinnovative.de>
2026-03-26 08:10:55 +0100

    fix telescope on macos and simplified/corrected the filetype command

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,36 +5,22 @@ return {
     build = ':TSUpdate',
     config = function()
         local ts = require('nvim-treesitter')
+        ts.install({
+            -- Structural & Config
+            'yaml', 'json', 'toml', 'ron', 'xml', 'csv', 'ini', 'dockerfile',
+            -- Scripting & Systems
+            'bash', 'fish', 'zsh', 'rust', 'python', 'lua', 'zig', 'go', 'gomod', 'c', 'cpp',
+            -- Web & App
+            'javascript', 'typescript', 'tsx', 'html', 'css', 'scss', 'java', 'kotlin', 'ruby',
+            -- Docs & Git
+            'markdown', 'markdown_inline', 'vim', 'vimdoc', 'query', 'diff',
+            'git_config', 'gitcommit', 'gitignore',
+        })
 
-        ts.install(
-            {
-                -- 1. Structural & Config (Essential for Markdown frontmatter & CLI)
-                'yaml', 'json', 'toml', 'ron', 'xml', 'csv', 'ini', 'dockerfile',
-
-                -- 2. Scripting & Systems (The "Must-Haves")
-                'bash', 'fish', 'zsh', 'rust', 'python', 'lua', 'zig', 'go', 'gomod', 'c', 'cpp',
-
-                -- 3. Web & App Development
-                'javascript', 'typescript', 'tsx', 'html', 'css', 'scss', 'java', 'kotlin', 'ruby',
-
-                -- 4. Documentation & Git (The "Invisible" Essentials)
-                'markdown', 'markdown_inline', 'vim', 'vimdoc', 'query', 'diff', 'git_config', 'gitcommit', 'gitignore'
-            }
-        )
-
-        -- IMPORTANT: In the 'main' branch of nvim-treesitter, you must manually trigger the highlighter.
-        -- This autocmd will start Tree-sitter highlighting whenever you open a supported file.
         vim.api.nvim_create_autocmd('FileType', {
             callback = function(args)
-                local buf = args.buf
-                local lang = vim.treesitter.language.get_lang(vim.bo[buf].filetype)
-
-                -- 1. Check if Neovim even knows this language
-                -- 2. Check if the parser is actually installed on your system
-                if lang and pcall(vim.treesitter.get_parser, buf, lang) then
-                    vim.treesitter.start(buf, lang)
-                end
+                pcall(vim.treesitter.start, args.buf)
             end,
         })
-    end
+    end,
 }
