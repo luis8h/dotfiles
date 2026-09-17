2e0c48a9302715bb0fb439e4d28cb97ddb4f3707
luis8h <git@luis8h.de>
2026-09-17 10:02:38 +0200

    add hlslens to nvim

--- .config/nvim/lua/luis8h/set.lua
+++ .config/nvim/lua/luis8h/set.lua
@@ -23,7 +23,7 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = false
+vim.opt.hlsearch = true -- changed to true to make hlslens work
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
